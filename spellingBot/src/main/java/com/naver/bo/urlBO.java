b2b3a51cf0df273c0a65c45c9e36c9ce50b4159c
jjh <jjh@localhost>
2016-07-11 11:06:40 +0900

    Test

--- spellingBot/src/main/java/com/naver/bo/urlBO.java
+++
@@ -1,8 +0,0 @@
-package com.naver.bo;
-
-public interface urlBO {
-
-	
-	public int urlAdd(String REAL_URL, String SHORT_URL);
-	
-}
--- spellingBot/src/main/java/com/naver/dao/urlDAO.java
+++
@@ -1,6 +0,0 @@
-package com.naver.dao;
-
-public interface urlDAO {
-
-	public int urlAdd(String REAL_URL, String SHORT_URL);
-}
